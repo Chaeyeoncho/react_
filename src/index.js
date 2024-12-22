54d53735072e00e57259b8e5d920d6e7a490a80e
Chaeyeoncho <gcy9293@naver.com>
2024-12-22 23:21:51 +0900

    innitial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {add, subtract} from './math.js';
+import multiply from './math.js';
+
+console.log(add(3, 2));
+console.log(subtract(2, 1));
+console.log('Hello, world!');
---
+++ src/math.js
@@ -0,0 +1,14 @@
+// math 모듈
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+export { add, subtract };
+
+export default function multiply(a, b) {
+    return a * b;
+    }
