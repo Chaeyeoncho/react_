db7f5fd908cf226fceac887b9ee115c2e2413c88
Chaeyeoncho <gcy9293@naver.com>
2024-12-24 01:22:03 +0900

    이벤트핸들러

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,9 +1,20 @@
-function Button( {text, color,children}){
-    return <button style = {{color :color }}>
-        {text} - {color}
-        {children}
+function Button({text, color,children}){
+    // e는 이벤트 객체를 의미합니다. 이벤트 객체는 이벤트가 발생했을 때 이벤트에 대한 정보를 가지고 있습니다.
+    const onClickButton = (e) => { 
+        console.log(e);
+        console.log(text);
+    };
+
+    return(
+         <button 
+            onClick = {onClickButton}
+            style = {{color :color }}
+            >
+            {text} - {color}
+            {children}
         </button>
-}
+    );
+};
 
 Button.defaultProps = {
     color : "blue"
