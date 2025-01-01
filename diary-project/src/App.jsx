b061e42b597f1a174094a1bcfa175890c0a6b2db
Chaeyeoncho <gcy9293@naver.com>
2025-01-01 14:00:08 +0900

    feat : 이미지 , 폰트 설정

--- diary-project/src/App.jsx
+++ diary-project/src/App.jsx
@@ -3,6 +3,13 @@ import Diary from './pages/Diary'
 import Home from './pages/Home'
 import New from './pages/New'
 import NotFound from './pages/NotFound'
+
+import emotion1 from './assets/emotion1.png'
+import emotion2 from './assets/emotion2.png'
+import emotion3 from './assets/emotion3.png'
+import emotion4 from './assets/emotion4.png'
+import emotion5 from './assets/emotion5.png'
+
 function App() {
   const nav = useNavigate()
 
@@ -12,6 +19,13 @@ function App() {
 
   return (
   <>
+  <div>
+    <img src = {emotion1} alt = "emotion1" />
+    <img src = {emotion2} alt = "emotion2" />
+    <img src = {emotion3} alt = "emotion3" />
+    <img src = {emotion4} alt = "emotion4" />
+    <img src = {emotion5} alt = "emotion5" />
+  </div>
   <div>
     <Link to="/">Home</Link>
     <Link to="/diary">Diary</Link>
